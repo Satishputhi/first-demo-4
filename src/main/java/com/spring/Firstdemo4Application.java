0c602fcc0cf26dae9ad61e1ec490f4bd92e564b3
Satishputhi <satishpooti05@gmail.com>
2023-08-30 10:37:32 +0530

    new one

---
+++ src/main/java/com/spring/Firstdemo4Application.java
@@ -0,0 +1,13 @@
+package com.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Firstdemo4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Firstdemo4Application.class, args);
+	}
+
+}
